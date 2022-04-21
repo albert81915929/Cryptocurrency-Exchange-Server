b0165a24f02e8eb58a8a0136f3f578eb4b9a4a82
Jt <76499116+albert81915929@users.noreply.github.com>
2022-04-21 13:25:53 -0400

    Add files via upload

--- send_tokens.py
+++ send_tokens.py
@@ -150,10 +150,11 @@ def send_tokens_eth(w3,sender_sk,txes):
         signed_tx = w3.eth.account.sign_transaction(tx_dict, sender_sk)
         try:
             tx_id = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-            # tx_ids.append(tx_id)
+            tx_ids.append(tx_id)
         except Exception as e:
             tx_id = None
 
-        tx_ids.append(tx_id)
+        # tx_ids.append(tx_id)
+        time.sleeo(1.0)
 
     return tx_ids
